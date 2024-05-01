e88c943365ebde8e89304d14c4be83ff9bfef7e9
Ethanwang17 <ejw17@uw.edu>
2024-05-01 15:03:51 -0700

    Initial Commit

---
+++ iQuiz/iQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  iQuizApp.swift
+//  iQuiz
+//
+//  Created by Ethan Wang on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct iQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
